534ce0dfe4b64a6687a690f261061604e98c77db
ashmit <ashmit@ashmit.ashmit>
2024-09-27 12:15:07 +0545

    Package

--- dist/routes/onboard.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getUserOnboards, postUserOnboards, patchUserOnboards, deleteUserOnboards } from "../controllers/onboard";
-const router = express.Router();
-router.route('/:userId').get(getUserOnboards).post(postUserOnboards).patch(patchUserOnboards).delete(deleteUserOnboards);
-export default router;
-//# sourceMappingURL=onboard.js.map
