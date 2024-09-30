704882e4bd79f047159a57ecee629fe8a9115c97
ashmit <ashmit@ashmit.ashmit>
2024-09-30 11:14:24 +0545

    uploads15

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,20 @@
 import express from 'express'
 import db from "../db/db"
-import fs from 'fs';
 import userRouter from "./routes/user"
-import https from 'https';
 import taskRouter from "./routes/task"
 import onboardRouter from "./routes/onboard"
 import { Request,Response } from 'express'
-import path from 'path'
 import cors from 'cors'
-
 
 const port = process.env.PORT || 8080
 const app = express()
 
-const options = {
-  key: fs.readFileSync(path.resolve(__dirname, '../server.key')), 
-  cert: fs.readFileSync(path.resolve(__dirname, '../server.cert')), 
-};
 
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://posthog-frontend.vercel.app', // Allow all origins
-  methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'],
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
@@ -36,13 +28,13 @@ app.get('/',(req:Request,res:Response)=>{
 res.send('App is running and uploads should run now!')
 })
 
-https.createServer(options, app).listen(port, async () => {
+app.listen(port,async() => {
   try {
     await db.select();
     console.log(`Server is running on port ${port}`);
   } catch (error) {
-    console.log(error);
+    console.log(error); 
   }
-});
+})
 
 
